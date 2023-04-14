e3089bc7e2a397c90b5e115f07654e974d70bdaf
Maja <majkatkaczykk@gmail.com>
2023-04-14 18:51:57 +0200

    added classes, threads, poison, eating and hedgehog

--- main.py
+++ main.py
@@ -1,67 +1,105 @@
 import pygame
 import random
+import threading
+import time
 
 pygame.init()
 window = pygame.display.set_mode((1050, 750))
 
-duckImg = pygame.image.load('assets/kaczuszka.png')
-appleImg = pygame.image.load('assets/apple.png')
-haps_yellow = pygame.image.load('assets/haps_yellow.png')
+duckImg = pygame.image.load("assets/kaczuszka.png")
+hedgehogImg = pygame.image.load("assets/hedgehog.png")
+appleImg = pygame.image.load("assets/apple.png")
+haps_yellow = pygame.image.load("assets/haps_yellow.png")
+poisonImg = pygame.image.load("assets/poison.png")
+eatenAppleImg = pygame.image.load("assets/eaten_apple.png")
 
 food_pos_x = [300, 450, 600]
-food_pos_y = [150, 300, 450]  
+food_pos_y = [150, 300, 450]
 
-def apple():
+
+class Apple:
+    def __init__(self, apple_x, apple_y):
+        self.apple_x = apple_x
+        self.apple_y = apple_y
+
+    def apple(self):
+        self.apple_x = random.choice(food_pos_x)
+        self.apple_y = random.choice(food_pos_y)
+        time.sleep(0.10)
+        window.blit(appleImg, (self.apple_x, self.apple_y))
+
+
+class Poison:
+    def __init__(self, poison_x, poison_y):
+        self.poison_x = random.choice(food_pos_x)
+        self.poison_y = random.choice(food_pos_y)
+
+    def poison(self):
+        self.poison_x = random.choice(food_pos_x)
+        self.poison_y = random.choice(food_pos_y)
+        # pygame.time.delay(140)
+        # time.sleep(0.15)
+        time.sleep(0.15)
+        window.blit(poisonImg, (self.poison_x, self.poison_y))
+
+
+class Game:
+    def __init__(self, poison: Poison, apple: Apple):
+        self.frame = pygame.rect.Rect(298, 148, 454, 454)
+        self.background_tile = pygame.rect.Rect(300, 150, 450, 450)
+        self.poison = poison
+        self.apple = apple
+
+    def start(self):
+        run = True
+
+        while run:
+            for event in pygame.event.get():
+                # close the window:
+                if event.type == pygame.QUIT:
+                    run = False
+
+            # add background color:
+            window.fill((130, 30, 30))
+            pygame.draw.rect(window, (255, 255, 255), self.frame)
+            pygame.draw.rect(window, (130, 30, 30), self.background_tile)
+
+            # print duck:
+            window.blit(duckImg, (150, 300))
+            window.blit(hedgehogImg, (750, 300))
+            appleThread = threading.Thread(target=self.apple.apple)
+            poisonThread = threading.Thread(target=self.poison.poison)
+            appleThread.start()
+            poisonThread.start()
+            appleThread.join()
+            poisonThread.join()
+
+            if pygame.key.get_pressed()[pygame.K_SPACE] and (
+                apple.apple_x == 300 and apple.apple_y == 300
+            ):
+                self.duckEats()
+
+            pygame.display.update()
+
+    def duckEats(self):
+        pygame.display.update()
+        apple.apple_x = 300
+        apple.apple_y = 300
+        window.blit(appleImg, (300, 300))
+        shape = pygame.rect.Rect(300, 300, 150, 150)
+        pygame.draw.rect(window, (130, 30, 30), shape)
+        window.blit(eatenAppleImg, (300, 300))
+        # time.sleep(2)
+        window.blit(haps_yellow, (150, 150))
+        pygame.time.wait(650)
+
+
+if __name__ == "__main__":
     apple_x = random.choice(food_pos_x)
     apple_y = random.choice(food_pos_y)
-    pygame.time.delay(140)
-    window.blit(appleImg, (apple_x, apple_y))
-
-# tiles (not necessary)
-frame = pygame.rect.Rect(298, 148, 454, 454)
-background_tile = pygame.rect.Rect(300, 150, 450, 450)
-# tile1 = pygame.rect.Rect(300, 150, 150, 150)
-# tile2 = pygame.rect.Rect(450, 150, 150, 150)
-# tile3 = pygame.rect.Rect(600, 150, 150, 150)
-# tile4 = pygame.rect.Rect(300, 300, 150, 150)
-# tile5 = pygame.rect.Rect(450, 300, 150, 150)
-# tile6 = pygame.rect.Rect(600, 300, 150, 150)
-# tile7 = pygame.rect.Rect(300, 450, 150, 150)
-# tile8 = pygame.rect.Rect(450, 450, 150, 150)
-# tile9 = pygame.rect.Rect(600, 450, 150, 150)
-
-# main loop:
-run = True
-while run:
-    for event in pygame.event.get():
-        # close the window:
-        if event.type == pygame.QUIT:
-            run = False
-
-    # add background color:
-    window.fill((130, 30, 30))
-    pygame.draw.rect(window, (255,255,255), frame)
-    pygame.draw.rect(window, (130, 30, 30), background_tile)
-
-    # draw tile1 [window, color, object]
-    # pygame.draw.rect(window, (120,0,0), tile1)
-    # pygame.draw.rect(window, (190,0,0), tile2)
-    # pygame.draw.rect(window, (255,0,0), tile3)
-    # pygame.draw.rect(window, (0,120,0), tile4)
-    # pygame.draw.rect(window, (0,190,0), tile5)
-    # pygame.draw.rect(window, (0,255,0), tile6)
-    # pygame.draw.rect(window, (0,0,120), tile7)
-    # pygame.draw.rect(window, (0,0,190), tile8)
-    # pygame.draw.rect(window, (0,0,255), tile9)
-    
-    #print duck:
-    window.blit(duckImg, (150, 300))
-
-    apple()
-
-    #spacja:
-    if pygame.key.get_pressed()[pygame.K_SPACE]:
-        window.blit(haps_yellow, (150, 150))
-
-
-    pygame.display.update()
+    poison_x = random.choice(food_pos_x)
+    poison_y = random.choice(food_pos_y)
+    apple = Apple(apple_x, apple_y)
+    poison = Poison(poison_x, poison_y)
+    game = Game(poison, apple)
+    game.start()
